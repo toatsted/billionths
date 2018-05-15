ba918dbb3262b2cb9bb422d8a486acd07b0042b1
Alan Salinas <alanrene19@gmail.com>
2018-05-15 12:42:43 -0500

    express, google auth

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+// Routes
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
