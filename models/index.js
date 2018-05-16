a437186482ffb47df9ac94b539c4952b6261b3ee
Alan Salinas <alanrene19@gmail.com>
2018-05-16 15:04:39 -0500

    server side auth

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { OAuth2Client } = require('google-auth-library');
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
@@ -8,19 +9,77 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/..\config\config.json')[env];
 var db        = {};
 
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = "480019328973-svpoqjokmkhv8s90kmhmt4qqvctbaco3.apps.googleusercontent.com"
-const client = new OAuth2Client(CLIENT_ID);
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const opn = require('opn');
 
-async function verify() {
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: CLIENT_ID
-  });
-  const payload = ticket.getPayload();
-  const userId = payload['sub'];
+const keys = require('./keys.json');
+
+/**
+ * Start by acquiring a pre-authenticated oAuth2 client.
+ */
+async function main() {
+  try {
+    const oAuth2Client = await getAuthenticatedClient();
+    // Make a simple request to the Google Plus API using our pre-authenticated client.
+
+    const url = 'https://people.googleapis.com/$discovery/rest?version=v1';
+    const res = await oAuth2Client.request({ url })
+    console.log(res.data);
+  } catch (e) {
+    console.error(e);
+  }
+  process.exit();
 }
-verify().catch(console.error);
+
+/**
+ * Create a new OAuth2Client, and go through the OAuth2 content
+ * workflow.  Return the full client to the callback.
+ */
+function getAuthenticatedClient() {
+  return new Promise((resolve, reject) => {
+    // create an oAuth client to authorize the API call.
+
+    const oAuth2Client = new OAuth2Client(
+      keys.web.client_id,
+      keys.web.client_secret,
+      keys.web.redirect_uris[0]
+    );
+
+    // Generate the url that will be used for the consent dialog.
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/plus.me'
+    });
+
+    // Open an http server to accept the oauth callback.
+
+    const server = http.createServer(async (req, res) => {
+      if (req.url.indexOf('/oauth2callback') > -1) {
+        // acquire the code from the querystring, and close the web server.
+        const qs = querystring.parse(url.parse(req.url).query);
+        console.log(`Code is ${qs.code}`);
+        res.end('Authentication successful! Please return to the console.');
+        server.close();
+
+        // Now that we have the code, use that to acquire tokens.
+        const r = await oAuth2Client.getToken(qs.code)
+        // Make sure to set the credentials on the OAuth2 client.
+        oAuth2Client.setCredentials(r.tokens);
+        console.info('Tokens acquired.');
+        resolve(oAuth2Client);
+      }
+    }).listen(3000, () => {
+      // open the browser to the authorize url to start the workflow
+      opn(authorizeUrl);
+    });
+  });
+}
+
+$("#google-sign-in").on("click", function() {
+  main();
+})
 
 
 if (config.use_env_variable) {
