a1aeb3d9eadad5f09da01453f7387f5ec2e8a4c4
Sam <samueljacksonhicks@gmail.com>
2018-05-16 19:46:23 -0500

    add catch statement

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
 
 	app.get("/googlesignin", (req, res) => {
-		require("../config/google.js")(); 
+		require("../config/google.js")()
+			.catch(err => send(err))
 	});
 }
