fecad7bb339d7c6dca62195ec0bd632213db3bdc
Sam <samueljacksonhicks@gmail.com>
2018-05-21 18:35:33 -0500

    handlebars

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -3,4 +3,12 @@ module.exports = function(app){
 	app.get("/", (req, res) => {
 		res.render("index");
 	})
+
+	app.get("/profile", (req, res) => {
+		res.render("profile");
+	})
+
+	app.get("/contact", (req, res) => {
+		res.render("contact");
+	})
 }
