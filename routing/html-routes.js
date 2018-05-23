957540af08f8cc1da2d15722d9efce94bce36f6f
Alan Salinas <alanrene19@gmail.com>
2018-05-23 13:44:59 -0500

    test

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -2,22 +2,18 @@ module.exports = function(app){
 		
 	app.get("/", (req, res, next) => {
 		res.render("index");
-		next();
 	})
 
 	app.get('/transactions', (req, res, next) => {
 		res.render("transactions");
-		next();
 	})
 
 	app.get("/index", (req, res, next) => {
 		res.render("dashboard");
-		next();
 	})
 
 	app.get("/contact", (req, res, next) => {
 		res.render("contact");
-		next();
 	})
 	
 }
