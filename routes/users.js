3ed02467cfa014395e1443ad7d6d74a4e934e2d8
Alan Salinas <alanrene19@gmail.com>
2018-05-29 16:24:48 -0500

    test

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,19 @@ var user;
 
 module.exports = function (app, passport) {
 
-	app.get("/index", function (req, res) {
-		res.render("index");
-	});
+    app.get("/index", function (req, res) {
+        res.render("index");
+    });
 
-	app.get("/logout", function (req, res) {
-		req.session.destroy(function (err) {
-			res.redirect("/");
-		});
-	});
+    app.get("/logout", function (req, res) {
+        req.session.destroy(function (err) {
+            res.redirect("/");
+        });
+    });
 
     app.get("/profile", function (req, res) {
         res.render("profile");
-        
+
     });
 
     function isLoggedIn(req, res, next) {
@@ -23,18 +23,18 @@ module.exports = function (app, passport) {
             return next();
     };
 
-	app.get("/authSuccess", function (req, res) {
-		res.redirect("/profile/" + req.user.id);
-	});
-			
+    app.get("/authSuccess", function (req, res) {
+        res.redirect("/profile/" + req.user.id);
+    });
+
     app.get('/auth/google',
-	    passport.authenticate('google', {
-	        scope: [
-	            'profile',
-	            "https://www.googleapis.com/auth/plus.login",
-	            "https://www.googleapis.com/auth/plus.me"
-	        ]
-	    }));
+        passport.authenticate('google', {
+            scope: [
+                'profile',
+                "https://www.googleapis.com/auth/plus.login",
+                "https://www.googleapis.com/auth/plus.me"
+            ]
+        }));
 
 
     app.get('/auth/google/callback',
@@ -47,14 +47,10 @@ module.exports = function (app, passport) {
             res.redirect('/profile');
         });
 
-	// Get user profile info
-	app.get("/api/user", (req, res) => {
+    // Get user profile info
+    app.get("/api/user", (req, res) => {
         res.send(user);
 
-	    }).then((dbUser) => {
-	        res.json(dbUser);
-	    }).catch((err) => {
-			res.json(err);
-		});
     });
+
 }
