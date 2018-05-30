df00fa9a74f31efc9a59fde48d66fc98c2ef8e8a
Alan Salinas <alanrene19@gmail.com>
2018-05-30 12:27:08 -0500

    test

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -25,14 +25,12 @@ $(document).ready(function () {
                 coin: transactions[i].coin,
                 coinId: transactions[i].coinId,
                 purchaseAmount: transactions[i].purchaseAmount,
-                purchasePrice: transactions[i].purchasePrice
+                purchasePrice: transactions[i].purchasePrice,
+                purchaseDate: transactions[i].createdAt
             };
 
-            $("#purchasedCryptos").prepend("<div class='card'><div class='card-header'>Transaction</div><div class='card-body'><div class='row'><div class='col'><h5 class='card-title'>" + purchase.coin + "</h5><p class='card-text'>Amount Purchased: " + purchase.purchaseAmount + "Purchase Price: " + purchase.purchasePrice + "</p></div></div></div></div>");
+            $("#purchasedCryptos").prepend("<div class='card'><div class='card-header'>" + purchase.coin + "</div><div class='card-body'><div class='row'><div class='col'><h5 class='card-title'>Purchased on: " + purchase.purchaseDate + "</h5><p class='card-text'>Amount Purchased: " + purchase.purchaseAmount + "  |  Purchase Price: $" + purchase.purchasePrice + "</p></div></div></div></div>");
         }
     }
-
-
-
 
 });
