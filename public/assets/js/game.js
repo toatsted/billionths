b7575985fd03fbd067981aba86f8d8dae9d4eb0e
Alan Salinas <alanrene19@gmail.com>
2018-05-30 10:21:52 -0500

    test

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
             purchaseAmount: 111,
         };
 
-        $.post("/api/transaction", transaction);
+        $.post("/api/transactions", transaction);
     };
 
 
